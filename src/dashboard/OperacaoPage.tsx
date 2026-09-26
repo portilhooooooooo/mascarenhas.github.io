8141b7e367c30accd14e99641daccdd139d84ffd
portilhooooooooo <gbrportilho@outlook.com>
2026-09-25 23:24:23 -0400

    feat(operacao): add metabase operation page

---
+++ src/dashboard/OperacaoPage.tsx
@@ -0,0 +1,8 @@
+import { MetabaseOperacaoEmbed } from './MetabaseOperacaoEmbed';
+import './operacaoMetabase.css';
+
+export function OperacaoPage() {
+  return <div className="operacao-metabase-page">
+    <MetabaseOperacaoEmbed />
+  </div>;
+}
